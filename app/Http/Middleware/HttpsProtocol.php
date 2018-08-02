e6a7aa8681ea26447358995c0123b87c0decdbe9
ahmedmohamed1101140 <ahmedmohamed1101140@outlook.com>
2018-08-02 14:08:37 +0200

    comment ssl handler

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,9 +15,9 @@ class HttpsProtocol
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && env('APP_ENV') === 'prod') {
-            return redirect()->secure($request->getRequestUri());
-        }
+//        if (!$request->secure() && env('APP_ENV') === 'prod') {
+//            return redirect()->secure($request->getRequestUri());
+//        }
         return $next($request);
     }
 }
