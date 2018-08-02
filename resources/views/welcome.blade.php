292a0dd72a2da27d118f813be725575681914a9b
ahmedmohamed1101140 <ahmedmohamed1101140@outlook.com>
2018-08-02 13:41:35 +0200

    demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{asset('img/favicon-32x32.png')}}">
-    <title>Laravel | Crawler Test</title>
+    <title>Laravel | Crawler Demo</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
