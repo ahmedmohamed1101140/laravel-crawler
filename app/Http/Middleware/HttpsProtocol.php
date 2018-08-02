28aff0fc8b26d96bab821edb6791fc4c75555ec5
ahmedmohamed1101140 <ahmedmohamed1101140@outlook.com>
2018-08-02 14:13:28 +0200

    https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,9 +15,10 @@ class HttpsProtocol
      */
     public function handle($request, Closure $next)
     {
-//        if (!$request->secure() && env('APP_ENV') === 'prod') {
-//            return redirect()->secure($request->getRequestUri());
-//        }
+        $url = parse_url(getenv("APP_ENV"));
+        if (!$request->secure() && $url === 'prod') {
+            return redirect()->secure($request->getRequestUri());
+        }
         return $next($request);
     }
 }
